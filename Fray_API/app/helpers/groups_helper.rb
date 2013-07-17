e65bf54e4b27b83c14c6400c81c333c9620cbe84
Robert Peralta <rapito@gmail.com>
2013-07-17 03:25:24 -0400

    Added Students table to Lectures Show Action

--- Fray_API/app/helpers/groups_helper.rb
+++ Fray_API/app/helpers/groups_helper.rb
@@ -2,4 +2,16 @@ module GroupsHelper
   def group_get_period group
     group.period.init_date.to_formatted_s(:long)  + ' - ' + group.period.end_date.to_formatted_s(:long)
   end
+
+  def group_get_name group
+
+    group.name+' - '+group.suffix
+
+  end
+
+  def group_get_long_name group
+
+    group.name+' - '+group.suffix+': '+group.stage.name
+
+  end
 end
--- Fray_API/app/helpers/schedules_helper.rb
+++ Fray_API/app/helpers/schedules_helper.rb
@@ -1,2 +1,5 @@
 module SchedulesHelper
+  def humanize_schedule schedule
+    schedule.day + ', ' + I18n.l( schedule.start_time, :format => "%I.%m %P").to_s + ' - ' + I18n.l( schedule.end_time, :format => "%I.%m %p")
+  end
 end
