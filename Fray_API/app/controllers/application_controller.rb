25cf4aeb4e616b6418ccf18daea18ee98c0c1e6b
Robert Peralta <rapito@gmail.com>
2013-07-18 03:17:47 -0400

    Added controls for Students and Teachers

--- Fray_API/app/controllers/application_controller.rb
+++ Fray_API/app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter  :set_locale
+  before_filter :set_locale
   before_filter :authenticate_user!
+  before_filter :check_session
 
   def set_locale
     locale = params[:locale]
@@ -15,4 +16,17 @@ class ApplicationController < ActionController::Base
     puts 'Logged out!'
   end
 
+  def check_session
+    available_for_students = [:groups,:lectures,:students,:teachers,:subjects]
+    action = :manage
+    puts params[:action]
+    available_for_students.each do |x|
+      if(params[:action] == action)
+        flash[:warning] = 'You are not authorized.'
+        redirect_to '/'
+        break
+      end
+    end
+
+  end
 end
